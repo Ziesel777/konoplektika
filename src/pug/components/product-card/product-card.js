7baaf5e9cc467b9f8c63abf7b46a93cebf07914e
Ziesel777 <ziesel777@gmail.com>
2019-12-12 23:36:35 +0500

    сопутствующие товары

--- src/pug/components/product-card/product-card.js
+++ src/pug/components/product-card/product-card.js
@@ -1,5 +1,6 @@
 import './card-gallery/card-gallery';
 import './card-buy-info/card-buy-info';
 import './card-desc/card-desc';
+import './card-addition/card-addition';
 
 console.log('hello world');
