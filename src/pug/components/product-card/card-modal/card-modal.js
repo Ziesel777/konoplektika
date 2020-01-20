6452d4a02c18f6b81708887e8eed0a6ed5daa9f4
Ziesel777 <ziesel777@gmail.com>
2020-01-20 12:28:17 +0500

    job отзыв

--- src/pug/components/product-card/card-modal/card-modal.js
+++ src/pug/components/product-card/card-modal/card-modal.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'magnific-popup';
 
 $(() => {
+// Окно соства продукта
     $('.js-modal-consist').magnificPopup({
 		type: 'inline',
 		preloader: false,
@@ -21,6 +22,7 @@ $(() => {
 		}
 	});
 
+// Окно отзыва
 	$('.js-modal-reviews').magnificPopup({
 		type: 'inline',
 		preloader: false,
@@ -40,4 +42,36 @@ $(() => {
 		}
 	});
 
+	$('.js-reviews-content').on('keyup focus change', function () {
+		let $btn = $(this).find('.js-reviews-btn');
+		$btn.prop('disabled',!validReviews(this));
+	});
+
+	$('.js-reviews-btn').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		fixedContentPos: true,
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
 });
+
+// проверка на заполненость окна отзыва
+function validReviews(selector){
+	let $name = $(selector).find('.js-reviews-userName');
+	let $comment = $(selector).find('.js-reviews-comment');
+	// let $btn = $(selector).find('.js-reviews-btn');
+
+	return $name.val().length>=3 && $comment.val().length>=10;
+}
